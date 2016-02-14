808a301ab6deea29875428b40956f5a4e389434e
Katya Fadeeva <katfadmail@gmail.com>
2016-02-15 01:11:02 +0200

    HelloClass

---
+++ src/me/fadeeva/Hello.java
@@ -0,0 +1,10 @@
+package me.fadeeva;
+
+/**
+ * Created by FK on 15.02.2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello here");
+    }
+}
